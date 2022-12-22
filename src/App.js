5336d4df2f63258f8778c53880fe69c8bc3f4fd1
MadsKodehode <mads.kodehode@gmail.com>
2022-12-22 19:12:17 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./styles/colorPalette.css"
+
+function App() {
+  return (
+    <div className="App">
+    <main className="main">
+      <aside className="greetings"></aside>
+    </main>
+    </div>
+  );
+}
+
+export default App;
